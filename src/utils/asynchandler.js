f0d0026d5e830ab8b11d4969a48afe305717b05e
Ram <rammohanofficial03@gmail.com>
2025-01-30 11:26:04 +0530

    Project setup done.

--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -1,6 +1,18 @@
-const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+// const asyncHandler = (requestHandler) => {
+//     (req, res, next) => {
+//         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+//     }
+// }
+
+
+const asyncHandler = (fn) => async(req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
     }
 }
 
@@ -8,14 +20,3 @@ export {asyncHandler}
 
 
 
-
-// const asyncHandler = (fn) => async(req, res, next) => {
-//     try {
-//         await fn(req, res, next)
-//     } catch (error) {
-//         res.status(error.code || 500).json({
-//             success: false,
-//             message: error.message
-//         })
-//     }
-// }
